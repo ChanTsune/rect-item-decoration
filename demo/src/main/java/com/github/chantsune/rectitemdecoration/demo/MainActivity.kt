e85cb6531a8c5a1223e2d2c1bf1d67a13b6653d9
ChanTsune <41658782+ChanTsune@users.noreply.github.com>
2021-02-23 22:55:42 +0900

    Merge branch 'gitlab-ci' into 'main'
    Gitlab ci
    
    See merge request s1f101702431/sample!2

--- demo/src/main/java/com/github/chantsune/rectitemdecoration/demo/MainActivity.kt
+++ demo/src/main/java/com/github/chantsune/rectitemdecoration/demo/MainActivity.kt
@@ -1,10 +1,10 @@
 package com.github.chantsune.rectitemdecoration.demo
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.AppCompatTextView
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -16,13 +16,17 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         findViewById<RecyclerView>(R.id.recycler_view).also { recyclerView ->
-            recyclerView.adapter = object: RecyclerView.Adapter<RecyclerView.ViewHolder>() {
-                inner class ViewHolder(view: View): RecyclerView.ViewHolder(view)
+            recyclerView.adapter = object : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+                inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view)
                 override fun onCreateViewHolder(
                     parent: ViewGroup,
                     viewType: Int
                 ): RecyclerView.ViewHolder {
-                    val view = LayoutInflater.from(parent.context).inflate(R.layout.view_item, parent, false)
+                    val view = LayoutInflater.from(parent.context).inflate(
+                        R.layout.view_item,
+                        parent,
+                        false
+                    )
                     return ViewHolder(view)
                 }
 
@@ -33,7 +37,6 @@ class MainActivity : AppCompatActivity() {
                 }
 
                 override fun getItemCount(): Int = 20
-
             }
             recyclerView.layoutManager = GridLayoutManager(this, 2)
             recyclerView.addItemDecoration(RectItemDecoration())
